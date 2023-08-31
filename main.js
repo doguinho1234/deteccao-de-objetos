a231d1bc517f45917260c0aa0db55d9b8a8a2cee
doguinho1234 <107889951+doguinho1234@users.noreply.github.com>
2023-08-31 18:44:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+img = "";
+objects = [];
+modelStatus = "";
+
+function preload() {
+    img = loadImage('ruas.jpg');
+}
+
+function setup () {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+   objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+}
+
+function modelLoaded() {
+    console.log("modelo foi carregado com sucesso!")
+    modelStatus = true;
+    objectDetector.detect(img, gotResult);
+}
+
+ function gotResult(error, results) {
+    if(error) {
+         console.log(error);
+     }
+     console.log(results);
+     objects = results;
+ }
+
+function draw() {
+image(img, 0, 0, 640, 420);
+
+if (modelStatus != "") {
+    for (i = 0; i < objects.length; i++) {
+        document.getElementById("h3").innerHTML = "Objetos Detectados"
+        fill("red");
+        percent = floor(objects[i].confidense * 100)
+        text(objects[i].label + " "+ percent + "%", objects[i].x, objects[i].y);
+        noFill()
+        stroke("red");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+}
+}
